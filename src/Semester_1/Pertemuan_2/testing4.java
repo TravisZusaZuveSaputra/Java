e2aa2489abf48b800e4d19436925d024a56bc821
Travis Zusa Zuve Saputra <traviszusa.2022@student.uny.ac.id>
2022-09-05 21:17:28 +0700

    5 September 2022

---
+++ src/Semester_1/Pertemuan_2/testing4.java
@@ -0,0 +1,21 @@
+package Semester_1.Pertemuan_2;
+
+public class testing4 {
+    public static void main(String[] args) {
+        open();
+        mid();
+        close();
+    }
+    public static void open() {
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+    }
+    public static void mid() {
+        System.out.println("-\"-'-\"-'-\"-");
+    }
+    public static void close() {
+        System.out.println("\\         /");
+        System.out.println();
+    }
+}
