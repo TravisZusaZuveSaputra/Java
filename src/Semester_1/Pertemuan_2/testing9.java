0c0fae2c8c4a3b61747cdc1d298b6b0f5f05ffb9
Travis Zusa Zuve Saputra <traviszusa.2022@student.uny.ac.id>
2022-09-22 14:18:01 +0700

    22 September 2022

---
+++ src/Semester_1/Pertemuan_2/testing9.java
@@ -0,0 +1,43 @@
+package Semester_1.Pertemuan_2;
+
+public class testing9 {
+    public static void main(String[] args) {
+        th();
+    }
+    public static void ten() {
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+    public static void hun() {
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+    }
+    public static void th() {
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+        hun();
+    }
+}
---
+++ src/Semester_1/Pertemuan_3/testing4.java
@@ -0,0 +1,9 @@
+package Semester_1.Pertemuan_3;
+
+public class testing4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6 ; ++i) {
+            System.out.println( i * 2 + (i + 5));
+        }
+    }
+}
