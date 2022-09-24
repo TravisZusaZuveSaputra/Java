636b02fdc93537316ad249cbdcd390fa60cec6ce
Travis Zusa Zuve Saputra <traviszusa.2022@student.uny.ac.id>
2022-09-24 21:28:18 +0700

    24 September 2022

---
+++ src/Semester_1/Pertemuan_3/Exercise12.java
@@ -0,0 +1,24 @@
+package Semester_1.Pertemuan_3;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j <= 5 - i; j++) {
+                System.out.print("-");
+            }
+            for (int j = 0; j <= a; j++) {
+                System.out.print(a);
+            }
+
+            a += 2;
+
+            for (int j = 0; j <= 5 - i; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+}
